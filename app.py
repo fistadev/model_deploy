0a680b43dd84c2d8a251635bc04983e04af534da
fistadev <fabiofistarol@gmail.com>
2021-07-15 17:27:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/model_page')
+# @app.route('/model_page')
 def model():
-    return Response(client())
+    return Response(client(), mimetype='text/html',)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run("127.0.0.1",  port=5011, debug=True)
