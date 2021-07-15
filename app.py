101623bcc26dbd351abe9a3e5ee4d56df68cadd7
fistadev <fabiofistarol@gmail.com>
2021-07-15 13:24:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+import pickle
 # import client
 
 app = Flask(__name__)
 
 # client = client()
+
+
+# Get model and predict
+
+# loaded_model = prepro.load_model()
+# pred = loaded_model.predict(x_test)
 
 
 @app.route("/")
