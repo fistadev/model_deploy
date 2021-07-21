c6ac3cc7180f45666bd130f912d84ff2e2df50de
fistadev <fabiofistarol@gmail.com>
2021-07-21 17:38:17 +0200

    Change HOST

--- client.py
+++ client.py
@@ -1,24 +1,24 @@
 import socket
 import numpy as np
-from model import X_test
+# from model import X_test
 
 
-#lst = [5.1, 3.5, 1.4, 0.2]
-#a = np.array(X_test)
-#a = a.tobytes()
+lst = [5.1, 3.5, 1.4, 0.2]
+a = np.array(lst)
+a = a.tobytes()
 
 
 def client():
-    HOST = '127.0.0.1'
-    PORT = 5010
+    HOST = '88.1.56.23'
+    PORT = 5011
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
     print('Client connected.')
     while True:
-        user_input = str(input(X_test))
+        user_input = str(input())
         if user_input == "exit":
             break
         mess = user_input.encode()
-        s.sendall(mess)
+        s.sendall(a)
 
     return s
